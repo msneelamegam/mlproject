d4c55b28c7bda12a3626a00679912465c62f34c9
neelamegam <msneelamegam@gmail.com>
2023-11-21 10:20:12 +0530

    Data Ingestion and Data Transformation

--- src/logger.py
+++ src/logger.py
@@ -5,7 +5,7 @@ from datetime import datetime
 LOGFILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path=os.path.join(os.getcwd(),"logs",LOGFILE)
 os.makedirs(logs_path,exist_ok=True)
-
+os.chdir('e:\workspace\')
 LOG_FILE_PATH=os.path.join(logs_path,LOGFILE)
 
 logging.basicConfig(
