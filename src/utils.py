4ab24b389bb67ea8ac06b63a6993c11674d9b9d8
neelamegam <msneelamegam@gmail.com>
2023-11-21 21:53:45 +0530

    model trainer

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,11 @@
+from ast import Param
 import os
 import sys
 
 import numpy as np
 import pandas as pd
 import dill
+from sklearn.metrics import r2_score
 #from src.exception import CustomException
 
 def save_object(file_path,obj):
@@ -16,3 +18,28 @@ def save_object(file_path,obj):
     except Exception as e:
         #raise CustomException(e,sys)
         print(e)
+
+def evaluate_models(x_train,y_train,x_test,y_test,models):
+    try:
+        report={}
+
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+            para=model.get_params()
+
+            model.fit(x_train,y_train)
+            y_train_pred=model.predict(x_train)
+
+            y_test_pred=model.predict(x_test)
+
+            train_model_score=r2_score(y_train,y_train_pred)
+
+            test_model_score=r2_score(y_test,y_test_pred)
+            
+            report[list(models.keys())[i]]=test_model_score
+
+        return report
+    except Exception as e:
+        print(e)
+
+
